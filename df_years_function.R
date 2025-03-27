6832e6e75abfab57e37f98ad497dfc6520df416f
SevastianSanchez <ss7257@columbia.edu>
2025-03-27 04:04:45 -0400

    update

--- df_years_function.R
+++ df_years_function.R
@@ -2,14 +2,14 @@
 #function to extract data from specified years 
 df_years <- function(name, df1=vdem, yr1=1999, #vdem data ONLY
                      df2=spi, yr2=yr1, #spi data ONLY
-                     df3=sdg, yr3=yr1) #sdg data ONLY 
-  { 
+                     df3=sdg, yr3=yr1, #sdg data ONLY
+                     df4=sci_df, yr4=yr1) { #sci data ONLY
   
   #VDEM DATASET
   
   name1 <- df1 %>%
     dplyr::select(country_name, country_text_id, year, v2x_regime, v2x_regime_amb, v2x_polyarchy, v2x_libdem, v2x_partipdem, v2x_delibdem, v2x_egaldem, v2xel_frefair, v2x_accountability, v2x_veracc, v2x_horacc, v2x_diagacc, v2xca_academ, v2x_freexp_altinf, e_gdp, e_gdppc, e_wb_pop, v3ststybcov, v3ststybpub, v3stcensus, v3ststatag) %>%
-    filter(year > yr1) %>% #Only 1999 and up 
+    filter(year >= yr1) %>% #Only 1999 and up 
     rename(country_code = country_text_id, #renaming country code (new_name = old_name)
            regime_type = v2x_regime, 
            regime_type_amb = v2x_regime_amb,
@@ -47,16 +47,28 @@ df_years <- function(name, df1=vdem, yr1=1999, #vdem data ONLY
     rename(p3_products = SPI.INDEX.PIL3) %>% #SPI pillar, 3 Data products  
     rename(p4_sources = SPI.INDEX.PIL4) %>% #SPI pillar, 4 Data sources 
     rename(p5_infra = SPI.INDEX.PIL5) %>% #SPI pillar 5, Data infrastructure
-    filter(year > yr2)
+    filter(year >= yr2)
   
   #SDG DATASET
-  
   name3 <- df3 %>% 
     dplyr::select("country_name", "country_code", "year", "sdg_overall", "goal1", "goal2", "goal3", "goal4", "goal5", "goal6", "goal7", "goal8", "goal9", "goal10", "goal11", "goal12", "goal3", "goal14", "goal15", "goal16", "goal17") %>% 
-    filter(year > yr3)
+    filter(year >= yr3)
    
-  #merge spi, sci, sdg and vdem data 
+  #SCI DATASET
+  name4 <- df4 %>% 
+    dplyr::select(country_name, country_code, Year, IQ.SCI.OVRL, IQ.SCI.MTHD, IQ.SCI.PRDC, IQ.SCI.SRCE) %>% 
+    filter(Year >= yr4) %>% 
+    rename(year = Year,
+           sci_overall = IQ.SCI.OVRL, 
+           sci_method = IQ.SCI.MTHD, 
+           sci_periodicity = IQ.SCI.PRDC,
+           sci_source = IQ.SCI.SRCE)
+  
+  name4$year <- as.numeric(name4$year)
+  
+  #MERGING spi, sci, sdg and vdem data 
   namex <- left_join(name3, name2, by = c("year", "country_code")) 
+  namex <- left_join(namex, name4, by = c("country_code", "year"))
   namex <- left_join(namex, name1, by = c("country_code", "year"))
   
   #rm duplicate col names 
@@ -65,10 +77,10 @@ df_years <- function(name, df1=vdem, yr1=1999, #vdem data ONLY
   #data type changes 
   namex$year <- as.integer(namex$year)
   name <- namex %>% 
-    mutate_at(c("spi_comp", "p1_use", "p2_services", "p3_products", "p4_sources", "p5_infra"), as.numeric)
+    dplyr::mutate_at(c("spi_comp", "p1_use", "p2_services", "p3_products", "p4_sources", "p5_infra", "sci_overall", "sci_method", "sci_periodicity", "sci_source"), as.numeric)
   
   return(name)
-}
+  }
 
-testingg <- df_years(yr1 = 2015)
+testing <- df_years(yr1 = 2014)
 
