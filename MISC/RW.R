96444254023f58ae3a7536eb8d7d489092aa5a28
SevastianSanchez <ss7257@columbia.edu>
2025-04-02 20:01:49 -0400

    update

---
+++ MISC/RW.R
@@ -0,0 +1,15 @@
+# Rough work 
+
+testing_df <- df_years(yr1 = 2005)
+
+vdemdata::find_var("gdp") #finds variables based on key words 
+
+
+region_info <- vdemdata::var_info("e_regionpol_6C") #info on variables 
+region_info$responses
+
+#backsliding:
+dem_breakdown_info <- vdemdata::var_info("e_democracy_breakdowns")
+
+#Democratization:
+dem_transition_info <- vdemdata::var_info("e_democracy_trans")
