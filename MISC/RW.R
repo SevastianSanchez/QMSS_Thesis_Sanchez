2570d5d7f87a7ca2a6556ac4b677a23cd9b9f006
SevastianSanchez <ss7257@columbia.edu>
2025-05-29 04:44:07 -0400

    update

--- MISC/RW.R
+++ MISC/RW.R
@@ -1,6 +1,41 @@
 # Rough work 
 
+# control/constant 
+test_lm1 <- lm(sdg_overall ~ spi_comp + aut_ep, data = event_data)
+summary(test_lm1) 
 
+# interaction
+test_lm2 <- lm(sdg_overall ~ spi_comp*aut_ep, data = event_data)
+summary(test_lm2) 
+
+# same as test_lm2
+test_lm3 <- lm(sdg_overall ~ spi_comp + spi_comp*aut_ep, data = event_data)
+summary(test_lm3) 
+
+# same as test_lm2
+test_lm3 <- lm(sdg_overall ~ spi_comp + aut_ep + spi_comp*aut_ep, data = event_data)
+summary(test_lm3) 
+
+# doesn't make sense - perplexity says remove 
+test_lm4 <- lm(sdg_overall ~ spi_comp:aut_ep, data = event_data)
+summary(test_lm4) 
+
+# spi_comp * aut_ep is the same as spi_comp + aut_ep + spi_comp:aut_ep
+test_lm5 <- lm(sdg_overall ~ spi_comp + aut_ep + spi_comp:aut_ep, data = event_data)
+summary(test_lm5) 
+
+# Perplexity: When aut_ep is binary (0/1), the coefficient shows the difference between the two groups. 
+# This is how binary variables always work in regression—they compare one group to the reference group.
+# Yes, it absolutely makes sense to interpret it as SPI's effect in different groups
+# That's the whole point of the interaction model! Here's how to read your results:
+
+# multiple categories - controls 
+test_ctrl <- lm(sdg_overall ~ spi_comp + income_level, data = merged_2015)
+summary(test_ctrl)
+
+# multiple categories - interaction 
+test_intr <- lm(sdg_overall ~ spi_comp*income_level, data = merged_2015)
+summary(test_intr)
 
 # New df with income classifications 
 read_csv("~/Documents/GitHub/QMSS_Thesis_Sanchez/Output_CSVs /world_bank_income_classifications.csv")
@@ -14,9 +49,6 @@ merged_inc_test <- merged_inc %>%
 testing_df <- df_years(yr1 = 2005)
 
 vdemdata::find_var("gdp") #finds variables based on key words 
-
-
-
 
 
 #Distribution of all countries that experienced democratization from 1900-2021
